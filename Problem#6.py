5794d45f528795371d99af6485f2aafd05504d66
shahan360 <sbsb4961@gmail.com>
2025-06-20 00:09:34 -0500

    completed Binary-Search-1

---
+++ Problem#6.py
@@ -0,0 +1,34 @@
+# Time Complexity : O(log n)
+# Space Complexity : O(1)
+# Did you run the code on Leetcode : Yes
+# Any problem you faced while coding this : No
+
+# Your code here along with comments explaining your approach in three sentences only
+
+
+'''
+33. Search in Rotated Sorted Array
+'''
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        if nums is None or len(nums) == 0: # We check if the list is empty or if the length of the list is zero
+            return -1 # If the list is empty, we return -1 as the target cannot be found
+        n = len(nums) # We store the length of the list in a variable n
+        low = 0 # We initialize low variable to 0, which represents the starting index of the list
+        high = n - 1 # We initialize high variable to n - 1, which represents the ending index of the list
+        while low <= high: # Until low index is less than or equal to high index we continue the search
+            mid = (low + high) // 2 # We calculate the mid index by taking the average of low and high indices
+            if nums[mid] == target: # If the mid index value is equal to the target value, we have found the target here itself
+                return mid # We return the mid index as the target is found at this index
+            elif nums[low] <= nums[mid]: # Left side is sorted # We check if the left side of the mid index is sorted
+                if nums[low] <= target < nums[mid]: # Then we further check in the left side that if the target is in the range of low and mid indices
+                    high = mid - 1 # If the target is in the range, we move the high index to mid - 1 to search in the left side
+                else: # If the target is not in the range, we move the low index to mid + 1 to search in the right side
+                    low = mid + 1 # We move the low index to mid + 1 to search in the right side
+            else: # Right side is sorted # If the left side is not sorted, then the right side must be sorted
+                if nums[mid] < target <= nums[high]: # We check if the target is in the range of mid and high indices
+                    low = mid + 1 # If the target is in the range, we move the low index to mid + 1 to search in the right side
+                else: # If the target is not in the range, we move the high index to mid - 1 to search in the left side
+                    high = mid -1 # We move the high index to mid - 1 to search in the left side
+        return -1   # If we exit the while loop, it means the target is not found in the list, so we return -1                           
---
+++ Problem#8.py
@@ -0,0 +1,34 @@
+# Time Complexity : O(log n)
+# Space Complexity : O(1)
+# Did you run the code on Leetcode : Yes
+# Any problem you faced while coding this : No
+
+# Your code here along with comments explaining your approach in three sentences only
+
+
+'''
+74. Search a 2D Matrix
+'''
+
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        if not matrix or not matrix[0]: # hum check karte hain agar matrix khali hai ya pehli row khali hai
+            return False # agar khali hai toh hum False return karte hain
+        m = len(matrix) # hum matrix ki rows ki sankhya ko m mein store karte hain (we store the length of the matrix or length of columns in m)
+        n = len(matrix[0]) # hum pehli row ki columns ki sankhya ko n mein store karte hain (we store the length of the first row or length of rows in n)
+
+        # ab hum binary search ka istemal karte hain (we use binary search)
+        left = 0 # hum left ko 0 se initialize karte hain (we initialize left to 0)
+        right = m * n - 1 # hum right ko m * n - 1 se initialize karte hain (we initialize right to m * n - 1)
+        while left <= right: # jab tak left chhota ya barabar hai right se (while left is less than or equal to right)
+            middle = (left + right) // 2 # hum middle ko left aur right ka average lete hain (we calculate the middle index)
+            idx_row = middle // n # hum middle ko n se divide karke row index nikaalte hain (we find the row index)
+            idx_col = middle % n # hum middle ko n se modulo karke column index nikaalte hain (we find the column index)
+            if matrix[idx_row][idx_col] == target: # agar matrix ke is index par value target ke barabar hai (if the value at this index in the matrix is equal to the target)
+                return True
+            else: # agar nahi hai (if not)
+                if matrix[idx_row][idx_col] < target: # agar matrix ke is index par value target se chhoti hai (if the value at this index in the matrix is less than the target)
+                    left = middle + 1 # toh hum left ko middle + 1 karte hain (we move left to middle + 1)
+                else: # agar matrix ke is index par value target se badi hai (if the value at this index in the matrix is greater than the target)
+                    right = middle - 1 # toh hum right ko middle - 1 karte hain (we move right to middle - 1)
+        return False # agar hum while loop se bahar nikalte hain, toh iska matlab target nahi mila (if we exit the while loop, it means the target was not found)
